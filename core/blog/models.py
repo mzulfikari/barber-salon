5b2cfc75bd7ff6e15b768cb276d5cab858d7b162
mzulfikari <mzulfikari1382@gmail.com>
2025-10-25 14:02:08 +0330

    change models services && Post

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
 
-# Create your models here.
+
+class CategoryStatus(models.IntegerChoices):
+    publish = 1 ,("نمایش")
+    draft = 2 ,("عدم نمایش")
+
+
+class CategoryPost(models.Model):
+    """ Categories of articles with the status of display and non-display by the admin"""
+    title = models.CharField(_('عنوان'),max_length=120)
+    status = models.IntegerField(_('وضعیت نمایش'),choices=CategoryStatus.choices,default=CategoryStatus.publish.value)
+    created_date = models.DateTimeField(_('تاریخ ایجاد'),auto_now_add=True)
+    
+    
+class CategoryGallery(models.Model):
+    """ Categories of Gallery with the status of display and non-display by the admin"""
+    title = models.CharField(_('عنوان'),max_length=120)
+    status = models.IntegerField(_('وضعیت نمایش'),choices=CategoryStatus.choices,default=CategoryStatus.publish.value)
+    created_date = models.DateTimeField(_('تاریخ ایجاد'),auto_now_add=True)
+    
+
+class Post(models.Model):
+    """   Model blog articles  """
+    title = models.CharField(_('عنوان'),max_length=120)
+    image = models.ImageField(_('تصویر پست'),upload_to='Post/image')
+    description = models.TextField(_('توضیحات'))
+    category = models.ForeignKey(CategoryPost,verbose_name='دسته بندی')
+    status = models.BooleanField(_('وضعیت نمایش'))
+    created_date = models.DateTimeField(_('تاریخ ایجاد'),auto_now_add=True)
+    updated_date = models.DateTimeField(_('تاریخ بروزرسانی'),auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def show_image(self):
+        """To display images in the management panel"""
+        if self.image:
+            return format_html(f'<img src="{self.image.url}" width="78 px" height="50" />')
+        return format_html('<h3 style="color: red">تصویر ندارد</h3>')
+    show_image.short_description = " تصاویر"
+
+
+    class Meta:
+        verbose_name_plural = "پست ها"
+        verbose_name = "پست"
+        ordering = ['-created']
+        
+
+class Gallery(models.Model):
+    """  Portfolio image gallery  """
+    title = models.CharField(_('عنوان'),max_length=120)
+    image = models.ImageField(_('تصویر پست'),upload_to='Gallery/image')
+    category = models.ForeignKey(CategoryGallery,verbose_name='دسته بندی گالری ها')
+    status = models.BooleanField(_('وضعیت نمایش'))
+    created_date = models.DateTimeField(_('تاریخ ایجاد'),auto_now_add=True)
+    updated_date = models.DateTimeField(_('تاریخ بروزرسانی'),auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def show_image(self):
+        """To display images in the management panel"""
+        if self.image:
+            return format_html(f'<img src="{self.image.url}" width="78 px" height="50" />')
+        return format_html('<h3 style="color: red">تصویر ندارد</h3>')
+    show_image.short_description = " تصاویر"
+    
+    class Meta:
+        verbose_name_plural = "پست ها"
+        verbose_name = "پست"
+        ordering = ['-created']
+        
+
+
+
--- core/service/models.py
+++ core/service/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+SERVICES = [
+    ('HAIRCUT', 'کوتاهی و استایل مو'),
+    ('HAIRCARE', 'احیا و مراقبت مو'),
+    ('HAIRCOLOR', 'رنگ، مش و هایلایت مو'),
+    ('HAIRSTYLE', 'طراحی مدل مو و براشینگ'),
+
+    ('BEARD_TRIM', 'اصلاح و فرم‌دهی ریش'),
+    ('BEARD_DESIGN', 'طراحی و خط‌گیری ریش و سبیل'),
+    ('BEARD_CARE', 'احیا و کراتینه ریش'),
+
+    ('FACIAL_BASIC', 'پاکسازی و مراقبت پوست'),
+    ('FACIAL_PRO', 'فیشیال تخصصی مردانه'),
+    ('EYEBROW', 'اصلاح و فرم‌دهی ابرو'),
+    ('MAKEUP_GROOM', 'گریم و آرایش داماد'),
+
+    ('MASSAGE', 'ماساژ بدن (ریلکسی / درمانی)'),
+    ('BODY_CARE', 'اسکراب و بخور بدن'),
+    ('EPILATION', 'اپیلاسیون بدن'),
+
+    ('GROOM_PACK', 'پکیج کامل داماد / VIP'),
+    ('VIP', 'خدمات ویژه و لوکس'),
+    ('SPECIAL', 'خدمات خاص و مدرن (نانو تراپی، کاشت مو و...)'),
+]
+
+
+class Service(models.Model):
+    """ 
+    Registration of hairdressing services with features of service time,
+    price,
+    description and image,
+    which is available for each separate reporting service
+    """
+    barber = models.ForeignKey('accounts.Profile',on_delete=models.CASCADE)
+    name = models.CharField(_('عنوان خدمت'),choices=SERVICES,max_length=50,related_name='name_service')
+    time = models.TimeField(_('زمان خدمت'))
+    image = models.ImageField(_('تصویر خدمت'),upload_to='service/image')
+    price = models.IntegerField(_('هزینه خدمت'))
+    description = models.TextField(_('توضیحات'))
+    status = models.BooleanField(_('وضعیت'))
+    create_date = models.DateTimeField(_('زمان اضافه کردن سرویس'),auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "خدمات"
+        verbose_name = "خدمت"
+
+
+class ReservationDay(models.Model):
+    """ Define the day in the database """
+    day = models.DateField(_('روز'),unique=True)
+    
+    def __str__(self):
+     return f"{self.day}"
+    
+
+class Reservation(models.Model):
+    """ 
+    Scheduling structure with the ability to choose the day of the appointment,
+    the hairdresser, the type of service, time and customer information
+    """
+    day = models.ForeignKey(ReservationDay,verbose_name='نوبت های روز')
+    barber = models.ForeignKey('accounts.Profile',on_delete=models.CASCADE)
+    service = models.ForeignKey(Service,verbose_name='خدمت')
+    time = models.DateTimeField(_('زمان ثبت نوبت'),auto_now_add=True)
+    full_name_customer = models.CharField(_('نام و نام خانوادگی مشتری'),max_length=100)
+    phone_customer = models.CharField(_('موبایل مشتری'),max_length=15,validators=[])
+
+    
+    def __str__(self):
+     return f"{self.day}  {self.service} "
+ 
+    class Meta:
+        unique_together = ('time','barber')
